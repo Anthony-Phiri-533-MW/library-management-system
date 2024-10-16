0188ae4374aaadccbfd934353f00f18822392e11
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2024-10-16 16:45:49 +0200

    Add readBooks function

--- main.cpp
+++ main.cpp
@@ -174,6 +174,89 @@ void writeBooksToFile(const string& title, const string& author, int ISBN, bool 
 }
 
 void addBooks(){
+    string title;
+    string author;
+    int ISBN;
+    bool status;
+
+    cout<<"Enter The book title"<<endl;
+    cin.ignore();
+    getline(cin,title);
+    //cin>>title;
+
+    cout<<"Enter the author"<<endl;
+    cin>>author;
+
+    cout<<"Enter the book's ISBN"<<endl;
+    cin>>ISBN;
+
+    cout<<"Enter the status(1 ro 0)"<<endl;
+    cin>>status;
+
+    writeBooksToFile(title,author,ISBN,status);
+}
+
+void readBooks() {
+    ifstream file("books.csv");
+    if (!file) {
+        cerr << "Error opening file." << endl;
+        return;
+    }
+    
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string title, author, statusStr;
+        int ISBN;
+        bool status;
+        
+
+
+        getline(ss, title, ',');
+        getline(ss, author, ',');
+        ss >> ISBN >> ws;
+        //ss >> status;
+        ss >> statusStr;
+
+        //status = (statusStr == "1");
+        
+        cout << "\nTitle: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "ISBN: " << ISBN << endl;
+        cout << "Status: " << (status ? "Available" : "Not Available") << endl;
+        cout << "---------------------------" << endl;
+    }
+
+    file.close();
+}
+
+void staffMenu(){
+    cout<<"STAFF SECTION"<<endl;
+    cout<<"1.Add books"<<endl;
+    cout<<"2.View all books"<<endl;
+    cout<<"2.Register a member"<<endl;
+
+    int staffOption;
+
+    cin>>staffOption;
+
+    switch (staffOption)
+    {
+    case 1:
+        {
+        addBooks();
+        break;
+
+        }
+    case 2:
+    {
+        readBooks();
+        break;
+    }
+    default:
+        break;
+    }
+    
     
 }
 
@@ -204,6 +287,7 @@ void mainMenu()
         if(stafflogin(username, password))
         {
             cout << "Login successful!" << endl;
+            staffMenu();
         }
         else
         {
